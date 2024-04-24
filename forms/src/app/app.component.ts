e191f72d9342dd93979ad88ea23b3c6af1a06da8
Samaneasyeat <saman@easyeat.ai>
2024-04-24 18:18:51 +0530

    "forms"

---
+++ forms/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myForm = new FormGroup({
+    firstName: new FormControl("",Validators.required),
+    lastName:new FormControl("", Validators.required),
+    address : new FormControl("",Validators.required),
+    mobileNum : new FormControl("",Validators.required)
+  })
+}
