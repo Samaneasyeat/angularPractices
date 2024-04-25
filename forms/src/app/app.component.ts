b3c71f701e84ca063f898137f8193017715ab724
Samaneasyeat <saman@easyeat.ai>
2024-04-25 12:42:58 +0530

    "validations for mobile number and more field"

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -10,9 +10,9 @@ export class AppComponent {
   myForm = new FormGroup({
     firstName: new FormControl("",[Validators.required,Validators.minLength(6)]),
     lastName:new FormControl("", [Validators.required,Validators.minLength(6)]),
-    address : new FormControl("",Validators.required),
-    mobileNum : new FormControl("",Validators.required),
-    companyName:new FormControl("",Validators.required),
+    address : new FormControl("",[Validators.required,Validators.minLength(12)]),
+    mobileNum : new FormControl("",[Validators.required,Validators.min(1000000000)]),
+    companyName:new FormControl("",[Validators.required,Validators.minLength(15)]),
     currentSalary:new FormControl("",Validators.required)
   })
 }
