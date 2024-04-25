81cd55ef038543b8eaf2b47bc7a06a3c64a47333
Samaneasyeat <saman@easyeat.ai>
2024-04-25 13:28:23 +0530

    "maxlength for fields"

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -8,11 +8,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   myForm = new FormGroup({
-    firstName: new FormControl("",[Validators.required,Validators.minLength(6)]),
-    lastName:new FormControl("", [Validators.required,Validators.minLength(6)]),
-    address : new FormControl("",[Validators.required,Validators.minLength(12)]),
-    mobileNum : new FormControl("",[Validators.required,Validators.min(1000000000)]),
-    companyName:new FormControl("",[Validators.required,Validators.minLength(15)]),
+    firstName: new FormControl("",[Validators.required,Validators.minLength(6),Validators.maxLength(15)]),
+    lastName:new FormControl("", [Validators.required,Validators.minLength(6),Validators.maxLength(15)]),
+    address : new FormControl("",[Validators.required,Validators.minLength(12), Validators.maxLength(20)] ),
+    mobileNum : new FormControl("",[Validators.required,Validators.min(1000000000),Validators.max(10000000000)]),
+    companyName:new FormControl("",[Validators.required,Validators.minLength(15),Validators.maxLength(25)]),
     currentSalary:new FormControl("",Validators.required)
   })
 }
