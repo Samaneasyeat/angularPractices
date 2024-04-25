0d97cc0e4977ddc29ccb4196b7fbf714d42b0ddb
Samaneasyeat <saman@easyeat.ai>
2024-04-25 11:38:30 +0530

    "minlength for last name "

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   myForm = new FormGroup({
     firstName: new FormControl("",[Validators.required,Validators.minLength(6)]),
-    lastName:new FormControl("", Validators.required),
+    lastName:new FormControl("", [Validators.required,Validators.minLength(6)]),
     address : new FormControl("",Validators.required),
     mobileNum : new FormControl("",Validators.required),
     companyName:new FormControl("",Validators.required),
